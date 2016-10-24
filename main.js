0cc8dc75c176ae5fadcc13210d8de7fd5760546c
awaisss <awaisilyas01@gmail.com>
2016-10-24 15:50:37 +0500

    Updates

---
+++ main.js
@@ -0,0 +1,14 @@
+var child_process = require('child_process');
+
+exports.handler = function(event,context) {
+	var proc = child_process.spawn('./main',[JSON.stringify(event)], {stdio:'inherit'});
+
+	proc.on('close',function(code){
+		if(code != 0){
+			return context.done(new Error("process exited with non zero status code"));
+		}
+		context.done(null);
+
+	});
+
+}
